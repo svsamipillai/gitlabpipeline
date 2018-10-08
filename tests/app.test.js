37b00db787740761b2858ae1161a7ae7d6584af8
Socratees Samipillai <socratees.samipillai@transcore.com>
2018-10-08 10:25:52 -0500

    Fix tests

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,5 +1,8 @@
+const expect = require("chai").expect;
+
 describe("app", () => { 
   it("a good test", () => {
-   expect(...).to.be....
+   expect({a: 1}).to.not.have.property('b');
+   expect([1, 2]).to.be.an('array').that.does.not.include(3);
   }) 
 });
